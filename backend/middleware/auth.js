fe54d79411b9b71a56278d3e0e555fe13167366b
Cody Marshall <codymstuntsx@gmail.com>
2022-11-07 11:09:34 -0800

    authorization for log in

---
+++ backend/middleware/auth.js
@@ -0,0 +1,15 @@
+module.exports.isAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+    res.status(404).json({ message: "Not authenticated" });
+    }
+  };
+  
+  module.exports.isAdmin = (req, res, next) => {
+    if (req.isAuthenticated() && req.user.admin) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  };
